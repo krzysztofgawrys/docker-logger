9c4063efab505a1a703bc647f786bd66bf7fc1b7
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-01-23 11:49:54 +0100

    tail added, update config

--- src/sites/Docker/docker.jsx
+++ src/sites/Docker/docker.jsx
@@ -7,7 +7,6 @@ const Docker = ({servers, match}) => {
     const docker = getDockerFromList(servers, id, index);
     return (
         <div>
-            <h1>Available servers</h1>
             <DockerComponent {...docker} />
         </div>
 
