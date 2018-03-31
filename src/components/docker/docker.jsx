31bef1874d14161f5728dd68cc988d2437d133e8
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-03-31 17:56:44 +0200

    fix errors

--- src/components/docker/docker.jsx
+++ src/components/docker/docker.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardTitle, CardText} from 'material-ui/Card';
 import {LazyStream, ScrollFollow} from 'react-lazylog/lib/LazyLog.es5';
 import FlatButton from 'material-ui/FlatButton';
+import {Row, Col} from 'react-grid-system';
+import {List, ListItem} from 'material-ui/List';
 import CloudDone from 'material-ui/svg-icons/file/cloud-done';
 import {green500} from 'material-ui/styles/colors';
+import NetworkIcon from 'material-ui/svg-icons/notification/network-check';
 import {parseName, networkParser} from '../../utils/parser';
 
 
@@ -12,11 +15,11 @@ const logsOutput = (id, URL) => {
     if (id) {
         ret = (
             <Card>
-                <CardTitle title="Logs from container" />
+                <CardTitle title="Logs from container"/>
                 <div className="logsSpace">
                     <ScrollFollow startFollowing>
                         {({follow, onScroll}) => (
-                            <LazyStream url={URL} follow={follow} onScroll={onScroll} />
+                            <LazyStream url={URL} follow={follow} onScroll={onScroll}/>
                         )}
                     </ScrollFollow>
                 </div>
@@ -39,16 +42,37 @@ const DockerComponent = (docker) => {
                         showExpandableButton
                         title={`${parseName(docker.name)} details`}
                         subtitle={`ID: ${docker.id}`}
-                        avatar={<CloudDone color={green500} />}
+                        avatar={<CloudDone color={green500}/>}
                     />
                     <CardText expandable>
-                        <p>Status: <strong>{docker.status}</strong></p>
-                        <p>Network (address): <strong>{network ? network.address : ''}</strong></p>
-                        <p>Network (gateway): <strong>{network ? network.gateway : ''}</strong></p>
+                        <Row>
+                            <Col sm={6} xs={12}>
+                                <List>
+                                    <ListItem
+                                        leftIcon={<CloudDone/>}
+                                        primaryText="Status"
+                                        secondaryText={docker.status}
+                                    />
+                                    <ListItem
+                                        leftIcon={<NetworkIcon/>}
+                                        primaryText="Network"
+                                        secondaryText={network ? network.address : ''}
+                                    />
+                                    <ListItem
+                                        leftIcon={<NetworkIcon/>}
+                                        primaryText="Gateway"
+                                        secondaryText={network ? network.gateway : ''}
+                                    />
+                                </List>
+                            </Col>
+                            <Col sm={6} xs={12}>
+                                Data about docker
+                            </Col>
+                        </Row>
                     </CardText>
                     <CardActions>
-                        <FlatButton label="restart" primary />
-                        <FlatButton label="stop" secondary />
+                        <FlatButton label="restart" primary/>
+                        <FlatButton label="stop" secondary/>
                     </CardActions>
                 </Card>
                 <div className="logs">
